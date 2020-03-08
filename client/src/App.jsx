67e0785ba6264b4ef16b572ed2df539f1543b7ac
Yousef <yousefrizik92@gmail.com>
2020-03-08 15:51:52 +0200

    adds file structure for client side

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {LandingPage, OrgInfo, Profile, SignUp, DonationPage} from './screens'
+
+function App () {
+    return (
+      <div>
+        <LandingPage/>
+      </div>
+    )
+}
+
+export default App
+
---
+++ client/src/screens/index.jsx
@@ -0,0 +1,9 @@
+import LandingPage from "./LandingPage";
+import OrgInfo from "./OrgInfo";
+import Profile from "./Profile";
+import SignUp  from "./SignUp";
+import DonationPage from "./DonationPage";
+
+
+
+export {LandingPage, OrgInfo, Profile, SignUp, DonationPage};
