bf6c826f2cc80c7cc9028d836b16eac25cccd048
Francis Aboud <francisaboud2000@gmail.com>
2020-03-11 12:40:42 +0200

    more fixes on branch userDetail

--- src/components/UserDetail/index.jsx
+++ src/components/UserDetail/index.jsx
@@ -13,70 +13,50 @@ function UserDetail() {
       labelName: "Full Name",
       value: fullName,
       type: "text",
-      onChange: event => {
-        handleFullNameChange(event.target.value);
-      },
-      onFocus: () => {
-        handleFullNameChange("");
-      }
+      onChange: event=>{handleFullNameChange(event.target.value)},
+      onFocus: (_,reset)=>{handleFullNameChange(reset?reset:'')}
     },
     {
       labelName: "Password",
       value: password,
       type: "password",
-      onChange: event => {
-        handlePasswordChange(event.target.value);
-      },
-      onFocus: () => {
-        handlePasswordChange("");
-      }
+      onChange: event=>{handlePasswordChange(event.target.value)},
+      onFocus: (_,reset)=>{handlePasswordChange(reset?reset:'')}
     },
     {
       labelName: "Address",
       value: address,
       type: "text",
-      onChange: event => {
-        handleAddressChange(event.target.value);
-      },
-      onFocus: () => {
-        handleAddressChange("");
-      }
+      onChange: event=>{handleAddressChange(event.target.value)},
+      onFocus: (_,reset)=>{handleAddressChange(reset?reset:'')}
     },
     {
       labelName: "City",
       value: city,
       type: "text",
-      onChange: event => {
-        handleCityChange(event.target.value);
-      },
-      onFocus: reset => {
-        handleCityChange(reset ? reset : "");
-      }
+      onChange: event=>{handleCityChange(event.target.value)},
+      onFocus: (_,reset)=>{handleCityChange(reset?reset:'')}
+      
     },
     {
       labelName: "PhoneNumber",
       value: phoneNumber,
       type: "text",
-      onChange: event => {
-        handlePhoneNumberChange(event.target.value);
-      },
-      onFocus: () => {
-        handlePhoneNumberChange("");
-      }
+      onChange: event=>{handlePhoneNumberChange(event.target.value)},
+      onFocus: (_,reset)=>{handlePhoneNumberChange(reset?reset:'')}
     }
   ];
-  let oldDetails = [fullName, password, address, city, phoneNumber];
+  const oldDetails=[fullName,password,address,city,phoneNumber]
 
   const editMode = () => {
-    setDisable(false);
+  setDisable(false)
   };
   const updateProfile = () => {
-    InputDivsInfo.map((objInput, i) =>
-      objInput.value === "" ? objInput.onFocus(oldDetails[i]) : ""
-    );
-
-    setDisable(true);
-  };
+    InputDivsInfo.map((objInput,i) => 
+      objInput.value===''?objInput.onFocus(null,oldDetails[i]):''
+    )
+    setDisable(true)
+    };
   return (
     <fieldset className="userDetail">
       <legend>User Details:</legend>
@@ -92,25 +72,21 @@ function UserDetail() {
         </button>
       </div>
       <form role="form">
-        {InputDivsInfo.map(objInput => (
-          <div className="form-group">
-            <label className="input_label">{objInput.labelName}: </label>
-            <input
-              value={objInput.value}
-              type={objInput.type}
-              onChange={objInput.onChange}
-              onFocus={objInput.onFocus}
-              className="form-control"
-              placeholder={`Enter your new ${objInput.labelName}`}
-              disabled={disabled}
-            />
-          </div>
-        ))}
-        {!disabled ? (
-          <button onClick={() => updateProfile()}>Update</button>
-        ) : (
-          ""
-        )}
+      {InputDivsInfo.map((objInput) => 
+        <div className="form-group">
+      <label className='input_label'>{objInput.labelName}: </label>
+      <input
+            value={objInput.value}
+            type={objInput.type}
+            onChange={objInput.onChange}
+            onFocus={objInput.onFocus}
+            className="form-control"
+            placeholder={`Enter your new ${objInput.labelName}`}
+            disabled={disabled}
+          />
+        </div>       
+      )}
+      {!disabled?<button onClick={() => updateProfile()}>Update</button>:''}
       </form>
     </fieldset>
   );
