130aa204d4c84687bfc8fa94afad7e05bebc9a38
Lina Qaddah <Lina.Qaddah@gmail.com>
2020-03-11 13:05:40 +0200

    modifying the style for the landing page

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import Player from "react-player";
+import './style.css'
 
 export class Video extends Component {
     render() {
         return (
-            <div>
-                <Player url="https://www.youtube.com/watch?v=Odg1BIfq60E"
+            <div className="video_div">
+               <center> 
+                   <Player width="90%" url="https://www.youtube.com/watch?v=Odg1BIfq60E"
                  autoPlay muted />
+                 </center>
             </div>
         )
     }
