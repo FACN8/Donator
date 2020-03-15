204fb6b19340bc97a9d5f9c12742a5b8b50c8996
Francis Aboud <francisaboud2000@gmail.com>
2020-03-15 10:16:24 +0200

    fixed @karyum issues

--- src/components/Chart/index.jsx
+++ src/components/Chart/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useTheme } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
-import './style.css'
+import "./style.css";
 import {
   Tooltip,
   Area,
@@ -12,7 +12,6 @@ import {
   ResponsiveContainer
 } from "recharts";
 import Typography from "@material-ui/core/Typography";
-// import Title from './Title';
 
 // Generate Sales Data
 function createData(donations, donationsByMonth) {
@@ -26,10 +25,10 @@ const data = [
   createData(15, 10),
   createData(20, 4),
   createData(25, 0),
-  createData(30, 6),
+  createData(30, 6)
 ];
 
-export default function Chart({filterMonth}) {
+export default function Chart({ filterMonth }) {
   const theme = useTheme();
   return (
     <Paper className="chart">
@@ -38,20 +37,28 @@ export default function Chart({filterMonth}) {
       </Typography>
 
       <ResponsiveContainer width={800} height={350}>
-      <AreaChart data={data}
-  margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
-  <defs>
-    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-      <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-      <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-    </linearGradient>
-  </defs>
-  <XAxis dataKey="donations" />
-  <YAxis />
-  <CartesianGrid strokeDasharray="3 3" />
-  <Tooltip />
-  <Area type="monotone" dataKey="donationsByMonth" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-</AreaChart>
+        <AreaChart
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="donations" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="donationsByMonth"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+        </AreaChart>
       </ResponsiveContainer>
     </Paper>
   );
